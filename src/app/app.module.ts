55715a87f353f7f46def1c9480c793ff3b24e2c1
Dennis Peters <dennis95peters@gmail.com>
2021-03-29 16:13:25 +0300

    This commit builds the components, classes and pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteListComponent } from './quote-list/quote-list.component';
+import { QuoteItemComponent } from './quote-item/quote-item.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteFormComponent,
+    QuoteListComponent,
+    QuoteItemComponent,
+    QuoteDetailsComponent,
+    DatePipePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes.ts
@@ -0,0 +1,13 @@
+export class Quotes {
+    constructor(
+      public id: number,
+      public quoteTitle: string,
+      public quoteText: string,
+      public authorName: string,
+      public userName: string,
+      public datePosted: Date,
+      public likes: number,
+      public dislikes: number,
+      public showDetails: boolean
+    ) {}
+  }
