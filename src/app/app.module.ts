05c59f7a4f03b86e7a1815fc530f2dd4506aa2bc
Dennis Peters <dennis95peters@gmail.com>
2021-03-30 05:48:45 +0300

    This removes some errors from the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
 import { QuoteListComponent } from './quote-list/quote-list.component';
 import { QuoteItemComponent } from './quote-item/quote-item.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DatePipePipe } from './date-pipe.pipe';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { DatePipePipe } from './date-pipe.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes';
 
+
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
